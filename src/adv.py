22a6b94ef91b6e9907ccc2703c4fa44f622a7480
tucker <tuckerwray84@gmail.com>
2020-07-15 00:01:35 -0400

    import sys do access player commandline args later

--- src/adv.py
+++ src/adv.py
@@ -1,3 +1,4 @@
+import sys
 from room import Room
 
 # Declare all the rooms
@@ -38,7 +39,7 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-
+playerOne=Player(outside)
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +50,4 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
