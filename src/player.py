cd733d223a0ada1124992130f86d35a11ae85139
tucker <tuckerwray84@gmail.com>
2020-07-15 00:02:34 -0400

    define base Player class in player.py. name currentToom health weapon description

--- src/player.py
+++ src/player.py
@@ -1,2 +1,7 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    def __init__(self, name, currentRoom, health, weapons, description, items=[]):
+        self.name = name
+        self.currentRoom-currentRoom
+        self.items
